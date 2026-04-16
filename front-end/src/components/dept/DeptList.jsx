bffaad534bfa07992c35f50709bc50bd5ef80eae
가람 <piyonoky@gmail.com>
2026-04-16 11:22:28 +0900

    소스검증

--- front-end/src/components/dept/DeptList.jsx
+++ front-end/src/components/dept/DeptList.jsx
@@ -30,7 +30,7 @@ const DeptList = () => {
     const res = await deptListDB(dept);
     console.log(res);
     setDeptList(res);
-    //console.log(res.data)
+    //console.log(res.data);
   };
 
   useEffect(() => {
--- front-end/src/components/dept/DeptRow.jsx
+++ front-end/src/components/dept/DeptRow.jsx
@@ -1,19 +1,19 @@
-import { Link } from "react-router"
+import { Link } from 'react-router';
 
-const DeptRow = ({dept}) => {
+const DeptRow = ({ dept }) => {
   return (
     <>
       <tr>
         <td>{dept.deptno}</td>
         <td>
-          <Link to={"/deptDetail/10"} className="btn btn-primary">
-          {dept.dname}
+          <Link to={'/deptDetail/10'} className='btn btn-primary'>
+            {dept.dname}
           </Link>
         </td>
         <td>{dept.loc}</td>
       </tr>
     </>
-  )
-}
+  );
+};
 
-export default DeptRow
+export default DeptRow;
